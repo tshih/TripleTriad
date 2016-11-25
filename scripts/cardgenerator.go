e8d2178b32b3e49d6d5c2d42f8acbc840e99182e
Timothy Shih <timothy.ts.shih@gmail.com>
2016-11-25 00:23:43 -0600

    Adding data parser

--- scripts/cardgenerator.go
+++ scripts/cardgenerator.go
@@ -1,17 +1,23 @@
 package scripts
 
 import (
+	"bufio"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"unicode"
 
 	"github.com/tshih/tripletriad/deck"
 )
 
-var dataPath = "./../data/"
+var DataPath = "./../data/"
 var imgPath = "./../img/"
+var jsonName = "cardData.json"
 
 type Level struct {
 	level int
@@ -29,24 +35,27 @@ func CreateCardJSON() int {
 		log.Fatal(err)
 	}
 
-	if val, _ := exists(dataPath); val {
-		os.RemoveAll(dataPath)
+	if val, _ := exists(DataPath + jsonName); val {
+		os.RemoveAll(DataPath + jsonName)
 	}
-	os.MkdirAll(dataPath, os.ModeDir)
+	//os.MkdirAll(DataPath, os.ModeDir)
 
 	levels := GenerateLevels(levelDirs)
 
 	cards := make([]deck.Card, 0, 110)
 
+	statMap := ParseStats(DataPath + "stats.txt")
+
 	for _, level := range levels {
 		for _, cardName := range level.cards {
 			card := deck.Card{ImgName: cardName, Level: level.level, Name: cardName[2 : len(cardName)-4]}
+			AddStats(&card, statMap)
 			cards = append(cards, card)
 		}
 	}
 
 	bytes, err := json.Marshal(cards)
-	err = ioutil.WriteFile("./../data/cardData.json", bytes, os.ModePerm)
+	err = ioutil.WriteFile(DataPath+jsonName, bytes, os.ModePerm)
 
 	return len(cards)
 	//names, err := os.Readdirnames(-1)
@@ -76,6 +85,93 @@ func GenerateLevels(dirs []os.FileInfo) []Level {
 	return levels
 }
 
+func ParseStats(filePath string) map[string][]int {
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	statMap := make(map[string][]int)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "Name") {
+			vals := strings.Split(line, ":")
+			stats := make([]int, 0, 4)
+
+			for scanner.Scan() {
+				// line 1
+				line2 := scanner.Text()
+				if strings.HasPrefix(line2, "Statistics") {
+					line = strings.Split(line2, ":")[1]
+					top, err1 := ConvertValue(line)
+					if err1 != nil {
+						log.Fatal(err1.Error())
+					}
+					stats = append(stats, top)
+
+					//line 2
+					scanner.Scan()
+					line2 = scanner.Text()
+					leftRight := strings.Split(strings.TrimSpace(line2), " ")
+					for _, str := range leftRight {
+						val, err2 := ConvertValue(str)
+						if err2 != nil {
+							continue
+						}
+						stats = append(stats, val)
+					}
+
+					scanner.Scan()
+					line2 = scanner.Text()
+					down, err3 := ConvertValue(line2)
+					if err3 != nil {
+						log.Fatal(err3)
+					}
+					stats = append(stats, down)
+					break
+				}
+
+			}
+			//fmt.Println(stats)
+			//fmt.Println(StripSpace(vals[1]))
+			statMap[StripSpace(vals[1])] = stats
+		}
+	}
+
+	return statMap
+
+}
+
+func AddStats(card *deck.Card, stats map[string][]int) {
+	vals, found := stats[card.Name]
+	if !found {
+		fmt.Printf("%v\n", card.Name)
+	} else {
+		fmt.Println(vals)
+	}
+}
+
+func ConvertValue(str string) (int, error) {
+	if len(str) == 0 {
+		return -1, errors.New("str length is 0")
+	}
+	str = strings.TrimSpace(str)
+	var val int
+	var err error
+	if str == "A" {
+		val = 10
+	} else {
+		val, err = strconv.Atoi(str)
+	}
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return val, nil
+}
+
 func exists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
@@ -85,4 +181,13 @@ func exists(path string) (bool, error) {
 		return false, nil
 	}
 	return true, err
+}
+
+func StripSpace(str string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) || unicode.IsSymbol(r) {
+			return -1
+		}
+		return r
+	}, str)
 }
--- scripts/cardgenerator_test.go
+++ scripts/cardgenerator_test.go
@@ -4,4 +4,11 @@ import "testing"
 
 func TestCardGenerator(t *testing.T) {
 	CreateCardJSON()
+	ParseStats(DataPath + "stats.txt")
+	// test := gosseract.Must(gosseract.Params{
+	// 	Src:       "./../img/Level1/TTBiteBug.png",
+	// 	Languages: "eng",
+	// })
+	//
+	// fmt.Println(test)
 }
